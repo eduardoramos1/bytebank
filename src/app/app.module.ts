1f7af129fb158c53e1044ca4963eb0fd2292c587
Eduardo Ramos <eduardorxs@gmail.com>
2021-06-05 18:58:23 -0300

    models, json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
@@ -21,7 +22,8 @@ registerLocaleData(localePt, 'pt')
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
---
+++ src/app/services/transferencia.service.ts
@@ -0,0 +1,35 @@
+import { Observable } from 'rxjs';
+import { Transferencia } from './../models/transferencia.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransferenciaService {
+  private listaTransferencia: any[] = []
+  private url = 'http://localhost:3000/transferencias'
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  get transferencias() {
+    return this.listaTransferencia
+  }
+
+  todas() : Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url)
+  }
+
+
+  adicionar(transferencia: any) {
+    this.lapidarDados(transferencia)
+    this.listaTransferencia.push(transferencia)
+  }
+
+  private lapidarDados(transferencia: any) {
+    transferencia.data = new Date();
+  }
+
+}
